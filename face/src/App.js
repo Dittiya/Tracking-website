1332944d4f56b983b108fbf2c10f12a594f50734
Aditiya <aditiyaart23@gmail.com>
2022-05-20 16:24:38 +0700

    init

--- face/src/App.js
+++ face/src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PRTS system online.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Welcome, Doctor _</h2>
       </header>
     </div>
   );
