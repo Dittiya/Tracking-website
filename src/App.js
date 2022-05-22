f0bf0e63b740a8001bd927a7177ce8e02e80bd5e
Aditiya <aditiyaart23@gmail.com>
2022-05-22 14:32:07 +0700

    refactor folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import logo from './assets/prts.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="prts" />
+        <h2>Call API</h2>
+        <h3>Typing test</h3>
       </header>
     </div>
   );
