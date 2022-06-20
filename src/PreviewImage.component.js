39f6484ac4582b6b52482267706a31a616be303f
Aditiya <aditiyaart23@gmail.com>
2022-06-20 23:43:08 +0700

    added PreviewImage component

---
+++ src/PreviewImage.component.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function PreviewImage() {
+  const [currentImg, setCurrentImg] = useState();
+
+  const changeImg = (e) => {
+    if (e.target.files && e.target.files.length > 0)
+      setCurrentImg(e.target.files[0]);
+  }
+
+  return (
+    <div className="container m-4">
+      <label for="image-file" className="mx-4">Upload image</label>
+      <input id="image-file" type="file" onChange={changeImg}></input>
+      {currentImg && (
+        <div className="container w-1/2 m-4">
+          <label className="text-2xl">Preview Image</label>
+          <img src={URL.createObjectURL(currentImg)} alt="alt" />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PreviewImage;
