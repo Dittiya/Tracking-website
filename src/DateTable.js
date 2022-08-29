9ebf360cc22e8541910d267818ae0f13730de5c2
Aditiya <aditiyaart23@gmail.com>
2022-08-29 21:38:11 +0700

    fetching events from cms

--- src/DateTable.js
+++ src/DateTable.js
@@ -16,21 +16,43 @@ function DateTable({ pulls }) {
 		const dateArr = [];
 
 		for (let i=0; i<dateRange; i++) {
-			const mo = date.getMonth() + i < 12 ? date.getMonth() + i : date.getMonth() + i - 12;
+			const mo = date.getMonth() + i < 12 ? 
+						      date.getMonth() + i : 
+						      date.getMonth() + i - 12;
 			dateArr.push(months[mo])
 		}
 
 		return dateArr;
 	}
 
-	function checkEvents() {
-		const events = [];
+	async function eventFetch() {
+		const url = 'https://raw.githubusercontent.com/Dittiya/Tracking-website/content-manager/public/cms.json';
+		const getter = await fetch(url);
 
-		for (let i=0; i<dates.length; i++) {
-			events.push('None');
-		}
+		return await getter.json();
+	}
 
-		return events;
+	async function checkEvents(today, date) {
+		const eventView = [];
+		const events = await eventFetch();
+
+		events['counter']['events'].forEach(e => {
+			const start = new Date(e['start']);
+      const end = new Date(e['end']);
+
+      if (today.getTime() <= end.getTime()) {
+        const idx = date.findIndex(e => e === months[start.getMonth()]);
+        eventView[idx] = e['name'];
+      }
+		});
+
+    for(let i=0; i<date.length; i++) {
+      if (!eventView[i]) eventView.push('None');
+    }
+
+    setNotables(eventView);
+
+    return eventView;
 	}
 
 	function calcForecast() {
@@ -50,6 +72,8 @@ function DateTable({ pulls }) {
 
 		setMonthlies(earningsMonthly);
 		setCerts(earningsMonthlyDistinction);
+
+		checkEvents(currentDate, date);
 
 		return 0;
 	}
